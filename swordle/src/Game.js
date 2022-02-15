976b1c2063f2fd1aa3c7621ff250273710e996ea
Stephanie Flower <stephanie.flower.15@gmail.com>
2022-02-15 14:24:38 +0000

    keyboard
    also player name variables added

--- swordle/src/Game.js
+++ swordle/src/Game.js
@@ -17,35 +17,39 @@ function Row(props) {
 }
 
 function Grid() {
+  var playerName = "Player 1";
   var rows = ['0', '1', '2', '3', '4', '5'];
   return (
-    <>
+    <> 
+      {playerName}
       <div className='grid'>{rows.map((row) => <Row rowId={row}/>)}</div>
     </>
   )
 }
 
 function EnemyGrid() {
+  var enemyName = "Player 2";
   var rows = ['0', '1', '2', '3', '4', '5'];
   return (
     <>
+      {enemyName}
       <div className='enemyGrid'>{rows.map((row) => <Row rowId={row}/>)}</div>
     </>
   )
 }
 
 function Key(props) {
-  return <div className="key" key={props.key}>{props.key}</div>
+  return <div key={props.currentKey}>{props.currentKey}</div>
 }
 
 function KeyBoard(){
   const keys = [
-    'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p',
-    'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l',
-    'z', 'x', 'c', 'v', 'b', 'n', 'm'
+    'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
+    'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '<-',
+    'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'ENTER'
   ];
 
-  return <div className ='keyBoard'>{keys.map((key) => <Key key={key}/>)}</div>
+  return <div className ='keyBoard'>{keys.map((key) => <Key currentKey={key}/>)}</div>
 }
 
 function Game(props) {
