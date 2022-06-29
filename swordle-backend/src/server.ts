27b8d616d71e54246d6d008e5438e9cfeb3051b3
Leo Durrant <poweredbykurisu@hotmail.com>
2022-06-29 20:44:09 +0100

    wip: converted project to TypeScript

---
+++ swordle-backend/src/server.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import * as http from "http";
+import * as path from "path";
+
+import { Server } from "socket.io";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./static/index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(5000, () => {
+  console.log("Express start on port 5000");
+});
